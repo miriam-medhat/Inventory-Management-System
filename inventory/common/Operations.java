1eba051b33f0d28d8e57cc7da3434a87079e34b1
feshjust45 <147548885+feshjust45@users.noreply.github.com>
2024-12-17 22:07:37 +0200

    Update Operations.java

--- inventory/common/Operations.java
+++ inventory/common/Operations.java
@@ -2,14 +2,14 @@ package inventory.common;
 
 import java.util.List;
 
-public interface Operations<T extends InventoryItem> {
+public interface Operations<T> extends InventoryItem> {
     void add(T item);
 
     void update(int id, T updatedItem);
 
     void delete(int id);
 
-    T getById(int id);
+    getById(int id);
 
     List<T> getAll();
 }
